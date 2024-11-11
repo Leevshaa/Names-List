7de2d4299a90937519e0e663fc6b9fdda31c2203
Vitalii <vitaliti.yurchenko@gmail.com>
2024-11-11 21:39:12 +0200

    Initial commit

---
+++ src/app/DataRepository.java
@@ -0,0 +1,14 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataRepository {
+
+    // Метод повертає список імен
+    public List<String> getData() {
+        List<String> list = ArrayList<>();
+
+        return list;
+    }
+}
